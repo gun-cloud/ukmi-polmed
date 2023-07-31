a0856df24128ab50d0ed1a68d02074d333dff567
HENDRI GUNAWAN <hendrigunawan@students.polmed.ac.id>
2023-07-31 10:43:16 +0700

    membuat halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function () {
-    return view('admin.index');
-});
 
+Route::get('/dashboard', [DashboardController::class,'index']);
